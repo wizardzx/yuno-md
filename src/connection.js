0a0269da9e7d5cb66fdbe68c4a3fb225cd4d3304
wizardzx <itx.nezuko@gmail.com>
2024-08-09 19:11:16 +0530

    ʏᴜɴᴏ ʙᴏᴛ🌷

---
+++ src/connection.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const { question, onlyNumbers } = require("./utils");
+const {
+  default: makeWASocket,
+  DisconnectReason,
+  useMultiFileAuthState,
+  fetchLatestBaileysVersion,
+} = require("@whiskeysockets/baileys");
+
+const pino = require("pino");
+
+exports.connect = async () => {
+  const { state, saveCreds } = await useMultiFileAuthState(
+    path.resolve(__dirname, "..", "assets", "auth", "baileys")
+  );
+
+  const { version } = await fetchLatestBaileysVersion();
+
+  const socket = makeWASocket({
+    printQRInTerminal: false,
+    version,
+    logger: pino({ level: "error" }),
+    auth: state,
+    browser: ["Ubuntu", "Chrome", "20.0.04"],
+    markOnlineOnConnect: true,
+  });
+
+  if (!socket.authState.creds.registered) {
+    const phoneNumber = await question("Informe o seu número de telefone: ");
+
+    if (!phoneNumber) {
+      throw new Error("Número de telefone inválido!");
+    }
+
+    const code = await socket.requestPairingCode(onlyNumbers(phoneNumber));
+
+    console.log(`Código de pareamento: ${code}`);
+  }
+
+  socket.ev.on("connection.update", (update) => {
+    const { connection, lastDisconnect } = update;
+
+    if (connection === "close") {
+      const shouldReconnect =
+        lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut;
+
+      if (shouldReconnect) {
+        this.connect();
+      }
+    }
+  });
+
+  socket.ev.on("creds.update", saveCreds);
+
+  return socket;
+};
